a820c6af50195b1cc19a22b3236542afdd6df9ce
Jakub <jakub.jaszcz538@gmail.com>
2022-09-12 13:39:49 +0200

    Added styling to my site, created public folder

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const bodyParser=require("body-parser");
 const express= require("express");
 const app=express();
+const path=require('path');
+
+app.use(express.static(__dirname + '/public'));
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html");
 });
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.listen(3000,()=>{
     console.log("Server running on port 3000")
 })
